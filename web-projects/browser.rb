4eb804d581aebb282e1183959675fa1ab3061f74
Davis Campbell <davis@campbellfamily5.com>
2015-08-10 16:58:23 -0400

    Browser and server work

--- web-projects/browser.rb
+++ web-projects/browser.rb
@@ -1,5 +1,8 @@
 require 'socket'
+require 'json'
 
+puts "Would you like to send a GET or a POST request?"
+verb = gets.chomp
 print "Enter hostname: "
 host = gets.chomp                   # The web server
 if host == 'localhost'
@@ -8,13 +11,27 @@ else
   port = 80
 end                                 # Default HTTP port
 print "Enter path: "
-path = gets.chomp                   # The file we want 
-
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+path = gets.chomp                   # The file we want
+print "\r\n"
 
 socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
+case verb
+when "GET"
+  request = "#{verb} #{path} HTTP/1.0\r\n\r\nEND"
+  socket.print(request)               # Send request
+  response = socket.read              # Read complete response
+when "POST"
+  puts "Welcome to raid registration. Please provide a name and e-mail address."
+  print "Name: "
+  name = gets.chomp
+  print "E-mail: "
+  email = gets.chomp
+  data = {:viking => {:name => name, :email => email} }.to_json
+  headers = ["#{verb} #{path} HTTP/1.0", "Content-Length: #{data.size}", "\r\n"]
+  request = headers.join("\r\n") + data + "END"
+  print request[0..-4] + "\r\n\r\n"
+  socket.print(request)
+  response = socket.read
+end
 
 print response
--- web-projects/server.rb
+++ web-projects/server.rb
@@ -1,22 +1,47 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(3000)
 loop do
   client = server.accept
-  request = client.gets
+  $/ = "END"
+  message = client.gets[0..-4]
+  $/ = "\n"
+  print message
+  request = message.split("\r\n")[0].chomp
   verb = request.split(' ')[0]
   path = request.split(' ')[1]
   version = request.split(' ')[2]
   response = []
-  begin
-    case verb
-    when "GET"
+  case verb
+  when "GET"
+    begin
       File.readlines(".#{path}", "r").each { |line| response.push(line) }
-      response = "#{version} 200 OK \r\n" + response.join("\r\n")
+      body = response.join("\r\n")
+      headers = ["#{version} 200 OK", "Date: #{Time.now.to_s}", "Content-Length: #{File.new(".#{path}", 'r').size}", "\r\n"]
+      response =  headers.join("\r\n") + body
+    rescue
+      response = "#{version} 404 Not Found\r\n"
     end
-  rescue
-    response = "#{version} 404 Not Found\r\n"
+  when "POST"
+    begin
+      params = JSON.parse(message.split("\r\n")[-1])
+      content = []
+      params['viking'].each do |key, value|
+        content.push("<li>#{key.capitalize}: #{value}</li>")
+      end
+      File.readlines(".#{path}", "r").each do |line|
+        line.gsub!("<%= yield %>", content.join("\r\n"))
+        response.push(line)
+      end
+      body = response.join("\r\n")
+      headers = ["#{version} 200 OK", "Date: #{Time.now.to_s}", "Content-Length: #{File.new(".#{path}", 'r').size}", "\r\n"]
+      response = headers.join("\r\n") + body
+#    rescue
+#      response = "#{version} 404 Not Found\r\n"
+    end
   end
   client.print response
   client.close
+print "\r\n\r\n"
 end
